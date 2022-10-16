4f9fcf01c5b4d64cae54fe4925f1cb48653a7e84
Eric Song <ericksong91@gmail.com>
2022-10-16 19:15:32 -0400

    done with code along

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,31 @@
-import React from "react";
+import { render } from "@testing-library/react";
+import React, {useEffect, useState} from "react";
+import QuestionItem from "./QuestionItem"
 
 function QuestionList() {
+  const [questions, setQuestions] = useState({})
+  const url = "http://localhost:4000/questions"
+
+  useEffect(()=>{
+    fetch(url)
+    .then((r)=>r.json())
+    .then((data)=>setQuestions(data))
+  }, [])
+
+  console.log(questions)
+
+  // function renderQuestions(arr){
+  //   const newArr = arr.map((question)=>{
+  //     <QuestionItem question={question} />
+  //   })
+  //   return newArr
+  // }
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      {/* <ul>{renderQuestions(questions)}</ul> */}
     </section>
   );
 }
